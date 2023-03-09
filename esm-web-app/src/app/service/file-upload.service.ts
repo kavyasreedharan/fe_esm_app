929f60bb70c7d0746a617999fbfcea2379beadd1
Kavya Sreedharan <kavya_s19@yahoo.com>
2023-03-09 11:19:22 +0800

    1) UI fixes for file upload screen 2) Error handling if server is down

--- esm-web-app/src/app/service/file-upload.service.ts
+++ esm-web-app/src/app/service/file-upload.service.ts
@@ -25,6 +25,7 @@ export class FileUploadService {
     if (error.status === 0) {
       // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred: ', error.error);
+      error.error.message = 'Unable to connect with server. Please try again later.'
     } else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong.
